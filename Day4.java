4a1c7d96b3d0f72d76885cbbea221411fd8ae5e5
moKa craft <mokacraft@gmail.com>
2019-12-04 22:36:19 +0100

    part2

--- Day4.java
+++ Day4.java
@@ -1,5 +1,3 @@
-package adventcode;
-
 public class Day4 {
 	private static final java.util.logging.Logger LOGGER = java.util.logging.Logger.getLogger(Day4.class.getName());
 
@@ -7,7 +5,7 @@ public class Day4 {
 	static final int HIGH_RANGE = 718098;
 
 	public static void main(String[] args) {
-		java.util.ArrayList<Integer> meetCriteria = new java.util.ArrayList<>();
+		java.util.ArrayList<Integer> meetCriteriaPart1 = new java.util.ArrayList<>();
 		for (int i = LOW_RANGE; i < HIGH_RANGE + 1; i++) {
 			int[] array = new int[6];
 			int value = i;
@@ -17,10 +15,38 @@ public class Day4 {
 				value /= 10;
 			}
 			if (hasAdjacentsSame(array) && neverDrecreases(array)) {
-				meetCriteria.add(i);
+				meetCriteriaPart1.add(i);
 			}
 		}
-		LOGGER.info("" + meetCriteria.size());
+		LOGGER.info("part1: " + meetCriteriaPart1.size());
+		java.util.ArrayList<Integer> meetCriteriaPart2 = new java.util.ArrayList<>();
+		for (int i : meetCriteriaPart1) {
+			int[] array = new int[6];
+			int value = i;
+			int index = 5;
+			while (value >= 1) {
+				array[index--] = value % 10;
+				value /= 10;
+			}
+			if (hasAtLeastTwoAdjacents(array)) {
+				meetCriteriaPart2.add(i);
+			}
+		}
+		LOGGER.info("part2: " + meetCriteriaPart2.size());
+	}
+
+	private static boolean hasAtLeastTwoAdjacents(final int[] value) {
+		if ((value[0] == value[1] && value[1] != value[2]) || (value[3] != value[4] && value[4] == value[5])) {
+			return true;
+		}
+		int i = 0;
+		while (i != 3) {
+			int j = i++;
+			if ((value[j] != value[j + 1] && value[j + 1] == value[j + 2] && value[j + 2] != value[j + 3])) {
+				return true;
+			}
+		}
+		return false;
 	}
 
 	private static boolean hasAdjacentsSame(final int[] value) {
